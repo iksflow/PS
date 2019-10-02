a738cc159e609a0660511f036d44e1db5dc21b54
rotcafer <rotcafer@gmail.com>
2019-10-02 23:50:03 +0900

    PS repository

---
+++ src/programmers/SkillCheck/P1988.java
@@ -0,0 +1,20 @@
+package programmers.SkillCheck;
+
+import java.time.LocalDate;
+
+public class P1988 {
+
+	public static void main(String[] args) {
+		solution(5, 6);
+		
+	}
+	
+	public static void solution(int a, int b) {
+		for (int i = 0; i < b; i++) {
+			for (int j = 0; j < a; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
