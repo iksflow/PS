44273ebb24d95be239599c73be2aafa5108c9995
rotcafer <rotcafer@gmail.com>
2019-10-10 22:10:58 +0900

    InsertionSort - Part1 solved

--- src/hackerRank/Test.java
+++ src/hackerRank/Test.java
@@ -4,7 +4,19 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		
+	}
+	
+	public static int introTutorial(int V, int[] arr) {
+		int answer = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (V == arr[i]) {
+				answer = i;
+				break;
+			}
+		}
+		
+		return answer;
 	}
 
 }
