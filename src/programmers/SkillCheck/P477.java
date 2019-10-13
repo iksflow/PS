1643a286f794e1f7bae79b4f92e2c7fb4187749c
rotcafer <rotcafer@gmail.com>
2019-10-13 20:26:41 +0900

    Lv2 Solving...

---
+++ src/programmers/SkillCheck/P477.java
@@ -0,0 +1,23 @@
+package programmers.SkillCheck;
+
+
+/**
+ * @author : pro
+ * @url : https://programmers.co.kr/skill_checks/56307?challenge_id=477
+ */
+public class P477 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.sqrt(2));
+	}
+	
+	public static boolean isPrimeNumber(int n) {
+		if (n%2 == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+}
