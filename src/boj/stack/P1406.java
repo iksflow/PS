e84adf22673c25bcbeca41cded40fadbcad6aa20
iksflow <iksflow@172.30.1.58>
2020-02-26 23:52:32 +0900

    first commit fromt MAC

---
+++ src/boj/stack/P1406.java
@@ -0,0 +1,16 @@
+package boj.stack;
+
+/**
+ * @Date		: 2020. 02. 26.
+ * @Problem Type: 
+ * @Problem No.	: 	
+ * @Problem Name: 
+ * @Elapsed Time: 
+ */
+public class P1406 {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
