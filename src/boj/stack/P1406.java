8ece8d3e8e396c30cafebf5ed226845c3500b512
rotcafer <rotcafer@gmail.com>
2020-02-28 22:22:33 +0900

    에디터(P1406) 해결

--- src/boj/stack/P1406.java
+++ src/boj/stack/P1406.java
@@ -1,24 +1,63 @@
 package boj.stack;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
- * @Date		: 2020. 02. 26.
+ * @Date		: 2020. 02. 28.
  * @Problem Type: Stack
  * @Problem No.	: 1406	
  * @Problem Name: 에디터
- * @Elapsed Time: 
+ * @Elapsed Time: 01:00:00
  */
 public class P1406 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println(solution(sc.nextLine()));
+		String inputText = sc.nextLine();
+		int numOfCommand = sc.nextInt();
+		sc.nextLine();
+		String[] commandArr = new String[numOfCommand];
+		for (int i = 0; i < numOfCommand; i++) {
+			commandArr[i] = sc.nextLine();
+		}
+		solution(inputText, commandArr);
+		sc.close();
 	}
 	
-	public static String solution(String str) {
-		StringBuilder result = new StringBuilder();
-		return result.toString();
+	public static void solution(String text, String[] commands) {
+		Stack<Character> leftOfCursor = new Stack<Character>();
+		Stack<Character> rightOfCursor = new Stack<Character>();
+		for (int i = 0; i < text.length(); i++) {
+			leftOfCursor.push(text.charAt(i));
+		}
+		for (String command : commands) {
+			switch(command.charAt(0)) {
+				case 'L':
+					if (!leftOfCursor.isEmpty()) {
+						rightOfCursor.push(leftOfCursor.pop());	
+					}
+					break;
+				case 'D':
+					if (!rightOfCursor.isEmpty()) {
+						leftOfCursor.push(rightOfCursor.pop());
+					}
+					break;
+				case 'B':
+					if (!leftOfCursor.isEmpty()) {
+						leftOfCursor.pop();
+					}
+					break;
+				case 'P':
+					leftOfCursor.push(command.charAt(2));
+					break;
+			}
+		}
+		while (!leftOfCursor.isEmpty()) {
+			rightOfCursor.push(leftOfCursor.pop());
+		}
+		while (!rightOfCursor.isEmpty()) {
+			System.out.print(rightOfCursor.pop());
+		}
 	}
-
 }
