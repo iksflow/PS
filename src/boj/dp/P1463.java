45f287f01d511d9d179b2b9622a351fc4c7df289
rotcafer <rotcafer@gmail.com>
2020-03-05 23:25:50 +0900

    해결완료

--- src/boj/dp/P1463.java
+++ src/boj/dp/P1463.java
@@ -19,7 +19,7 @@ public class P1463 {
 		System.out.println(solution(num));
 	}
 	
-	public static int solution(int target) {
+	public static int solution(int n) {
 		/*
 		 * D[N] = N을 1로만드는 최소연산횟수
 		 * D[N] 은 아래 3가지 중 최소값인 경우 1개로 표현할 수 있을것이다. 
@@ -28,22 +28,26 @@ public class P1463 {
 		 * D[N] = D[N-1] + 1
 		 * D[N] = MIN(D[N/3], D[N/2], D[N-1]) + 1;
 		 * */
-		int[] memo = new int[100];
-		memo[1] = 0;
-		if (target == 1) {
+		int[] d = new int[100];
+		if (n == 1) {
 			return 0;
 		}
-		if (memo[target] > 0) {
-			return memo[target];
-		} else if (target % 2 == 0) {
-			memo[target] = solution(target / 2) + 1; 
-			return memo[target];
-		} else if (target % 3 == 0) {
-			memo[target] = solution(target / 3) + 1;
-			return memo[target];
-		} else {
-			memo[target] = solution(target - 1) + 1;
-			return memo[target];
+		if (d[n] > 0) {
+			return d[n];
+		} 
+		d[n] = solution(n - 1) + 1;
+		if (n % 2 == 0) {
+			int temp = solution(n / 2) + 1;
+			if (d[n] > temp) {
+				d[n] = temp;
+			}
 		}
+		if (n % 3 == 0) {
+			int temp = solution(n / 3) + 1;
+			if (d[n] > temp) {
+				d[n] = temp;
+			}
+		}
+		return d[n];
 	}
 }
