66cf5705243ec2e9a2d52ffd65769976afc898ab
성승익 <seunglk.seong>
2020-03-05 18:07:12 +0900

    solving

--- src/boj/dp/P1463.java
+++ src/boj/dp/P1463.java
@@ -15,12 +15,35 @@ public class P1463 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = br.read();
+		int num = Integer.parseInt(br.readLine());
 		System.out.println(solution(num));
 	}
 	
 	public static int solution(int target) {
-		return 0;
+		/*
+		 * D[N] = N을 1로만드는 최소연산횟수
+		 * D[N] 은 아래 3가지 중 최소값인 경우 1개로 표현할 수 있을것이다. 
+		 * D[N] = D[N/3] + 1
+		 * D[N] = D[N/2] + 1
+		 * D[N] = D[N-1] + 1
+		 * D[N] = MIN(D[N/3], D[N/2], D[N-1]) + 1;
+		 * */
+		int[] memo = new int[100];
+		memo[1] = 0;
+		if (target == 1) {
+			return 0;
+		}
+		if (memo[target] > 0) {
+			return memo[target];
+		} else if (target % 2 == 0) {
+			memo[target] = solution(target / 2) + 1; 
+			return memo[target];
+		} else if (target % 3 == 0) {
+			memo[target] = solution(target / 3) + 1;
+			return memo[target];
+		} else {
+			memo[target] = solution(target - 1) + 1;
+			return memo[target];
+		}
 	}
-
 }
---
+++ src/boj/solving/P1406.java
@@ -0,0 +1,17 @@
+package boj.solving;
+
+
+/**
+ * @Date		: 2020. 3. 03.
+ * @Problem Type: DP
+ * @Problem No. : 1406
+ * @Problem Name: 1로 만들기
+ * @Elapsed Time: 01:00:00
+ */
+public class P1406 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
