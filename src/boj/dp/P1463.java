00b081956d269ecfd3cee67a72bd6d34cbbd4130
rotcafer <rotcafer@gmail.com>
2020-03-06 01:24:05 +0900

    메모리 문제 해결 필요

--- src/boj/dp/P1463.java
+++ src/boj/dp/P1463.java
@@ -1,8 +1,7 @@
 package boj.dp;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * @Date		: 2020. 3. 3.
@@ -14,22 +13,18 @@ import java.io.InputStreamReader;
 public class P1463 {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = Integer.parseInt(br.readLine());
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int num = Integer.parseInt(br.readLine());
+//		System.out.println(solution(num));
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
 		System.out.println(solution(num));
 	}
 	
 	public static int solution(int n) {
-		/*
-		 * D[N] = N을 1로만드는 최소연산횟수
-		 * D[N] 은 아래 3가지 중 최소값인 경우 1개로 표현할 수 있을것이다. 
-		 * D[N] = D[N/3] + 1
-		 * D[N] = D[N/2] + 1
-		 * D[N] = D[N-1] + 1
-		 * D[N] = MIN(D[N/3], D[N/2], D[N-1]) + 1;
-		 * */
+	
 		int[] d = new int[100];
-		if (n == 1) {
+		if (n < 2) {
 			return 0;
 		}
 		if (d[n] > 0) {
---
+++ src/boj/solving/Main.java
@@ -0,0 +1,40 @@
+package boj.solving;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int[] d = new int[num+1];
+		System.out.println(solution(num, d));
+	}
+	
+	public static int solution(int n, int[] d) {
+	
+		
+		if (n == 1) {
+			return 0;
+		}
+		if (d[n] > 0) {
+			return d[n];
+		} 
+		d[n] = solution(n - 1, d) + 1;
+		if (n % 2 == 0) {
+			int temp = solution(n / 2, d) + 1;
+			if (d[n] > temp) {
+				d[n] = temp;
+			}
+		}
+		if (n % 3 == 0) {
+			int temp = solution(n / 3, d) + 1;
+			if (d[n] > temp) {
+				d[n] = temp;
+			}
+		}
+		return d[n];
+	}
+}
