0a40f3b6c6e36c9a5ce4c7091dc6b7c67260bcd1
iksflow <theiksflow@gmail.com>
2020-03-09 17:57:53 +0900

    test

--- src/boj/solving/Main.java
+++ src/boj/solving/Main.java
@@ -4,35 +4,46 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*
+ * 정수 4를 1, 2, 3의 합으로 나타내는 방법은 총 7가지가 있다. 합을 나타낼 때는 수를 1개 이상 사용해야 한다.
+ * 1+1+1+1
+ * 1+1+2
+ * 1+2+1
+ * 2+1+1
+ * 2+2
+ * 1+3
+ * 3+1
+ * 정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
+ * [입력]
+ * 첫째 줄에 테스트 케이스의 개수 T가 주어진다. 
+ * 각 테스트 케이스는 한 줄로 이루어져 있고, 정수 n이 주어진다. n은 양수이며 11보다 작다.
+ * [출력]
+ * 각 테스트 케이스마다, n을 1, 2, 3의 합으로 나타내는 방법의 수를 출력한다.
+ */
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
-		int[] d = new int[num + 1];
+		int[] d = new int[num + 2];
 		System.out.println(solution(num, d));
 	}
+	/* 2×n 직사각형을 2×1(가로, 세로)과 2×2 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+	 * D[N] = D[N-1] + D[N-2] + D[N-2]
+	 * D[1] = 1
+	 * D[2] = 3
+	 * D[3] = D[2] + D[1]
+	 */
 	
 	public static int solution(int n, int[] d) {
-		if (n == 1) {
-			return 0;
-		}
-		if (d[n] > 0) {
+		int result = 0;
+		d[1] = 1;
+		d[2] = 3;
+		if (n < 3) {
 			return d[n];
-		} 
-		d[n] = solution(n - 1, d) + 1;
-		if (n % 2 == 0) {
-			int temp = solution(n / 2, d) + 1;
-			if (d[n] > temp) {
-				d[n] = temp;
-			}
 		}
-		if (n % 3 == 0) {
-			int temp = solution(n / 3, d) + 1;
-			if (d[n] > temp) {
-				d[n] = temp;
-			}
-		}
-		return d[n];
+		d[n] = solution(n - 1, d) + solution(n - 2, d) + solution(n - 2, d);
+		result = d[n] % 10007;
+		return result;
 	}
 }
