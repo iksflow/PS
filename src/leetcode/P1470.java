eee199bd4a2f479a542506a94fcfa39d75eda47b
iksflow <theiksflow@gmail.com>
2020-07-22 18:04:57 +0900

    문제풀이

--- src/leetcode/P1470.java
+++ src/leetcode/P1470.java
@@ -1,4 +1,16 @@
 package leetcode;
 
 public class P1470 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] shuffle(int[] nums, int n) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[i+n];
+        }
+        return result;
+    }
 }
