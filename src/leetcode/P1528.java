83fd96116ee2e439d200fb67555475b3d647638d
rotcafer <rotcafer@gmail.com>
2020-07-26 23:27:19 +0900

    P1528번 해결

---
+++ src/leetcode/P1528.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class P1528 {
+
+    public static void main(String[] args) {
+        char[] str = {'a', 'b', 'c'};
+        System.out.println(String.copyValueOf(str));
+    }
+
+    public String restoreString(String s, int[] indices) {
+        char[] str = new char [s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            str[indices[i]] = s.charAt(i);
+        }
+        return new String(str);
+
+    }
+}
+
