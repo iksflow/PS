74c66e924f5ed4c4caa9288b527bd12cf7b44ef8
rotcafer <rotcafer@gmail.com>
2020-07-30 23:46:56 +0900

    1021 풀이중

--- src/leetcode/P1021.java
+++ src/leetcode/P1021.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Stack;
+
 public class P1021 {
     public static void main(String[] args) {
 
@@ -7,6 +9,24 @@ public class P1021 {
     }
 
     public String removeOuterParentheses(String S) {
+        Stack<Character> stack = new Stack<>();
+        int[] arr = new int[S.length()];
+        int lcount = 0;
+        int rcount = 0;
+        for (int i = 0 ; i < S.length(); i++) {
+            if (S.charAt(i) == '(') {
+                if (0 < lcount) {
+
+                } else {
+                    lcount++;
+                }
+
+            }
+            if (S.charAt(i) == ')') {
+//                count--;
+            }
+
+        }
 
         return "";
     }
