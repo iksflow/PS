871c6bdc4674885b0c15f26dd98f6d88c13d3bee
rotcafer <rotcafer@gmail.com>
2020-08-01 22:43:36 +0900

    daily challenge 풀이

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -8,20 +8,30 @@ import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) {
-        List<Map<String, Object>> config = new ArrayList<>();
-        Map<String, Object> map1 = new HashMap<String, Object>();
-        map1.put("MCLS_CD", "1");
-        Map<String, Object> map2 = new HashMap<String, Object>();
-        map2.put("MCLS_CD", "2");
-        Map<String, Object> map3 = new HashMap<String, Object>();
-        map3.put("MCLS_CD", "3");
-        config.add(map1);
-        config.add(map2);
-        config.add(map3);
+        String word = "USA";
 
-        Map<String, Object> map = config.stream()
-                .map(o -> o.get("MCLS_CD").toString())
-                .collect(Collectors.toMap(s -> s.toString(), o -> o.length()));
-        System.out.println(map.toString());
+        System.out.println(detectCapitalUse(word));
+
+    }
+
+    public static boolean detectCapitalUse(String word) {
+
+        if (word.equals(word.toUpperCase())) {
+            return true;
+        }
+        if (word.equals(word.toLowerCase())) {
+            return true;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(0) <= 90) {
+                if (1 <= i && word.charAt(i) <= 90) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
