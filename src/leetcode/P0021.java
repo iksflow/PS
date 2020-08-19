3d81c131271a891faba5350819b28f78fb4bf01d
iksflow <theiksflow@gmail.com>
2020-08-19 17:59:12 +0900

    2020.08.19 Leetcode Study

--- src/leetcode/P0021.java
+++ src/leetcode/P0021.java
@@ -1,34 +1,67 @@
 package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /* Merge Two Sorted Lists */
 public class P0021 {
     public static void main(String[] args) {
+        ListNode a3 = new ListNode(4);
+        ListNode a2 = new ListNode(2, a3);
+        ListNode a1 = new ListNode(1, a2);
+        ListNode b3 = new ListNode(4);
+        ListNode b2 = new ListNode(3, b3);
+        ListNode b1 = new ListNode(1, b2);
+
+        ListNode result = mergeTwoLists(a1, b1);
+        System.out.println(result.toString());
+        while (result != null) {
+            System.out.println("result: " + result.val);
+
+            result = result.next;
+        }
 
     }
 
 
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-        while (l1.next != null) {
-            if (l1.next == null) {
-                l1.next = l2;
+        Queue<ListNode> q = new LinkedList<>();
+        ListNode next = null;
+        ListNode head = null;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                q.offer(l2);
+                l2 = l2.next;
+                continue;
+            }
+            if (l2 == null) {
+                q.offer(l1);
+                l1 = l1.next;
+                continue;
+            }
+            if (l1.val < l2.val) {
+                q.offer(l1);
+                l1 = l1.next;
+            } else {
+                q.offer(l2);
+                l2 = l2.next;
             }
         }
 
+        while (q.isEmpty() == false) {
 
 
+            if (head == null) {
+                head = q.poll();
+                next = head;
+                continue;
+            }
+            next.next = q.poll();
+            next = next.next;
 
+        }
 
-
-        return result;
+        return head;
     }
 }
--- src/leetcode/P0101.java
+++ src/leetcode/P0101.java
@@ -1,64 +1,73 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class P0101 {
     public static void main(String[] args) {
+//        TreeNode t7 = new TreeNode(3);
+//        TreeNode t6 = new TreeNode(4);
+//        TreeNode t5 = new TreeNode(4);
+//        TreeNode t4 = new TreeNode(3);
+//        TreeNode t3 = new TreeNode(2, t6, t7);
+//        TreeNode t2 = new TreeNode(2, t4, t5);
+//        TreeNode t1 =
 
+        TreeNode t15 = new TreeNode(8);
+        TreeNode t14 = new TreeNode(9);
+        TreeNode t13 = new TreeNode(0);
+        TreeNode t12 = new TreeNode(0);
+        TreeNode t11 = new TreeNode(9);
+        TreeNode t10 = new TreeNode(8);
+        TreeNode t9 = new TreeNode(0);
+        TreeNode t8 = new TreeNode(0);
+
+        TreeNode t7 = new TreeNode(4, t14, t15);
+        TreeNode t6 = new TreeNode(5, t12, t13);
+        TreeNode t5 = new TreeNode(5, t10, t11);
+        TreeNode t4 = new TreeNode(4, t8, t9);
+        TreeNode t3 = new TreeNode(3, t6, t7);
+        TreeNode t2 = new TreeNode(3, t4, t5);
+        TreeNode t1 = new TreeNode(2, t2, t3);
+        System.out.println(isSymmetric(t1));
     }
 
-    String[] check = new String[100];
-
-    public boolean isSymmetric(TreeNode root) {
-        if (root.left.val == root.right.val) {
-//            isSymmetric()
-        } else {
-            return false;
-        }
-
-
-        return true;
+    public static boolean isSymmetric(TreeNode root) {
+//        List<Integer> nodeList = bfs(root);
+//        int treeLevel = 1;
+//        int startIndex = 0;
+//        int endIndex = 0;
+//        while (Math.pow(2, treeLevel) - 1 < nodeList.size
+//        ()) {
+//            treeLevel++;
+//        }
+//        System.out.println("treeLevel: " + treeLevel);
+//
+//        for (int i = 0; i < treeLevel; i++) {
+//            startIndex
+//        }
+        return false;
     }
 
 
-    public void bfs(TreeNode root) {
+    public static List<Integer> bfs(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         while (queue.isEmpty() == false) {
             TreeNode temp = queue.poll();
+
             if (temp != null) {
-                queue.offer(temp.left);
-                queue.offer(temp.right);
+                result.add(temp.val);
+                if (temp.left != null || temp.right != null) {
+                    queue.offer(temp.left);
+                    queue.offer(temp.right);
+                }
+            } else {
+                result.add(null);
             }
 
         }
+
+        return result;
     }
-
-    public void compareTree(TreeNode t1, TreeNode t2) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(t1);
-        while (queue.isEmpty() == false) {
-            TreeNode temp = queue.poll();
-            if (temp != null) {
-
-
-            }
-
-        }
-    }
-
-
-    public void reverseTree(TreeNode root) {
-        if (root != null) {
-            TreeNode temp = root.left;
-            root.left = root.right;
-            root.right = temp;
-            reverseTree(root.left);
-            reverseTree(root.right);
-        }
-    }
-
 }
