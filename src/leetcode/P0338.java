99cf293719e30417837d9bbc7f9b6268774ce5a5
rotcafer <rotcafer@gmail.com>
2020-08-22 22:58:13 +0900

    Leetcode Study 08.22

---
+++ src/leetcode/P0338.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class P0338 {
+    public static void main(String[] args) {
+        System.out.println(5 ^ 0);
+    }
+
+    public int[] countBits(int num) {
+        int[] res = new int[num + 1];
+        // 홀수 짝수 이용
+
+        for (int i = 0; i <= num; i++) {
+            if (i % 2 == 0) res[i] = res[i / 2];
+            else res[i] = 1 + res[i-1];
+        }
+        return res;
+    }
+}
