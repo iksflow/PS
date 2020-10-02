cd8c4ba6f088d35261301a7e4aca8bd3ef847d08
iksflow <theiksflow@gmail.com>
2020-10-02 22:30:24 +0900

    2020.10.0122 Leetcode Study

---
+++ src/leetcode/P0476.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import sun.jvm.hotspot.oops.LongField;
+
+public class P0476 {
+    public static void main(String[] args) {
+        System.out.println(findComplement(2));
+
+
+    }
+
+    public static int findComplement(int num) {
+        long res = 0;
+        long max = 1;
+        while(max <= num) {
+            max *= 2;
+        }
+        res = max -1 -num;
+        return (int)res;
+    }
+}
