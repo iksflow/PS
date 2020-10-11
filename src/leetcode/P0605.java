9e870c90b18b73ea948996a3e245278bed89ab65
rotcafer <rotcafer@gmail.com>
2020-10-11 20:34:21 +0900

    Leetcode Study 10.11

---
+++ src/leetcode/P0605.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class P0605 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        return true;
+    }
+}
