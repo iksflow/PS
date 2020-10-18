c1380423671a61868f39f4a470f8d21a4743bb3f
rotcafer <rotcafer@gmail.com>
2020-10-18 22:57:18 +0900

    Leetcode Study 10.18

---
+++ src/leetcode/P0796.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class P0796 {
+    public static void main(String[] args) {
+
+    }
+    // using que
+    public boolean rotateString(String A, String B) {
+        return false;
+    }
+}
