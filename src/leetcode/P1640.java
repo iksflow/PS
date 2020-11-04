c4b58fb79120d4ecb6cfd43bf7be3593c8e239dc
iksflow <theiksflow@gmail.com>
2020-11-04 18:05:25 +0900

    2020.11.04 Leetcode Study P1640

---
+++ src/leetcode/P1640.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class P1640 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        return false;
+    }
+}
