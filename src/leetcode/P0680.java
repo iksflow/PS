ecc9a16277fed02a6618cbfb565b9fab7e8f6f11
iksflow <theiksflow@gmail.com>
2020-11-09 18:12:50 +0900

    2020.11.09 Leetcode Study / Solving P0680

---
+++ src/leetcode/P0680.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class P0680 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean validPalindrome(String s) {
+        return true;
+    }
+}
