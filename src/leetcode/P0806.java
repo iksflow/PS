10def18ab2c7de75610fdd812ab721dc8f6d8ebb
iksflow <theiksflow@gmail.com>
2020-12-02 21:20:53 +0900

    Leetcode Study 2020.12.02 / P0806

---
+++ src/leetcode/P0806.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class P0806 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] numberOfLines(int[] widths, String S) {
+        return null;
+    }
+}
