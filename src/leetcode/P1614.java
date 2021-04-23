684d353c8f4bac5715071d676540b8b17669d32b
iksflow <theiksflow@gmail.com>
2021-04-23 23:52:28 +0900

    Leetcode Study 2021.04.23 / P1614 String parsing

--- src/leetcode/P1614.java
+++ src/leetcode/P1614.java
@@ -1,11 +1,26 @@
 package leetcode;
 
+import java.util.Stack;
+
 public class P1614 {
     public static void main(String[] args) {
-
+        System.out.println(maxDepth("(1+(2*3)+((8)/4))+1"));
     }
 
     public static int maxDepth(String s) {
-        return 0;
+        Stack<Character> stack = new Stack<>();
+        char[] charr = s.toCharArray();
+        int max = 0;
+        for (char c : charr) {
+            if (c == '(') {
+                stack.push(c);
+            }
+            if (c == ')') {
+                stack.pop();
+            }
+            max = Math.max(max, stack.size());
+        }
+
+        return max;
     }
 }
