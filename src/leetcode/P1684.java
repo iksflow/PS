66132f5a11dccc5f4396c9004d2951e07d579dc1
iksflow <theiksflow@gmail.com>
2021-04-27 23:52:48 +0900

    Leetcode Study 2021.04.27 / P1684

---
+++ src/leetcode/P1684.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class P1684 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int countConsistentStrings(String allowed, String[] words) {
+        return 0;
+    }
+}
