f0e084414898cb41c3e137ede98c84e9337d127a
iksflow <theiksflow@gmail.com>
2021-04-28 23:35:54 +0900

    Leetcode Study 2021.04.28 / P1588 Solving

--- src/leetcode/P1588.java
+++ src/leetcode/P1588.java
@@ -2,11 +2,20 @@ package leetcode;
 
 public class P1588 {
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 3};
+        System.out.println(sumOddLengthSubarrays(arr));
     }
 
-
+    // [1, 4, 2, 5, 3]
+    // [1, 2]
     public static int sumOddLengthSubarrays(int[] arr) {
-        return 0;
+        int sum = 0;
+        int t = ((arr.length & 1) == 1) ? arr.length / 2 : arr.length / 2 + 1;
+        for (int i = 0; i < arr.length; i++) {
+            int step = arr.length - Math.abs(t - i);
+            sum += arr[i] * step;
+            System.out.println("current sum: " + arr[i] * step);
+        }
+        return sum;
     }
 }
