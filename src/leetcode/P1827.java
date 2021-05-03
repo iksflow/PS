0125907b91a81ea2363987521c44f40de2760d70
iksflow <theiksflow@gmail.com>
2021-05-03 19:25:32 +0900

    Leetcode Study 2021.05.03 / P1827 solving

---
+++ src/leetcode/P1827.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class P1827 {
+    public static void main(String[] args) {
+        int[] nums = {};
+        minOperations(nums);
+    }
+
+    public static int minOperations(int[] nums) {
+        return 0;
+    }
+}
