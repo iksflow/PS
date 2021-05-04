8a22209733d59669f6e7f2b3eb727f0dfb0d46e6
iksflow <theiksflow@gmail.com>
2021-05-04 23:32:57 +0900

    Leetcode Study 2021.05.03 / P1827 solving

--- src/leetcode/P1827.java
+++ src/leetcode/P1827.java
@@ -4,9 +4,12 @@ public class P1827 {
     public static void main(String[] args) {
         int[] nums = {};
         minOperations(nums);
+
     }
 
     public static int minOperations(int[] nums) {
+
+
         return 0;
     }
 }
