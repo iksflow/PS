52e039ec400c3c8ed1beed58e1ae759b16edfa6e
iksflow <theiksflow@gmail.com>
2021-05-05 22:50:35 +0900

    Leetcode Study 2021.05.03 / P1827 해결. 복습 필요

--- src/leetcode/P1827.java
+++ src/leetcode/P1827.java
@@ -2,14 +2,21 @@ package leetcode;
 
 public class P1827 {
     public static void main(String[] args) {
-        int[] nums = {};
-        minOperations(nums);
+        int[] nums = {1, 5, 2, 4, 1};
+        System.out.println(minOperations(nums));
 
     }
 
     public static int minOperations(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            while (nums[i-1] >= nums[i]) {
+                nums[i]++;
+                count++;
+            }
 
+        }
 
-        return 0;
+        return count;
     }
 }
