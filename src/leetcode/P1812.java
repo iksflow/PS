d524b193d808a40abe5e45974a035db85860c522
iksflow <theiksflow@gmail.com>
2021-05-07 22:29:45 +0900

    Leetcode Study 2021.05.03 / P1827 해결. 복습 필요

---
+++ src/leetcode/P1812.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class P1812 {
+    public static void main(String[] args) {
+        String coordinates = "a1";
+        System.out.println(squareIsWhite(coordinates));
+    }
+
+    public static boolean squareIsWhite(String coordinates) {
+        return true;
+    }
+}
