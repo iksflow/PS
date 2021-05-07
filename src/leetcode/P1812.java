79f2807103b89fb4b1d44186e5cf30fb7ea183c1
iksflow <theiksflow@gmail.com>
2021-05-07 22:41:14 +0900

    Leetcode Study 2021.05.06 / P1812 해결.

--- src/leetcode/P1812.java
+++ src/leetcode/P1812.java
@@ -2,12 +2,14 @@ package leetcode;
 
 public class P1812 {
     public static void main(String[] args) {
-        String coordinates = "a1";
-
+        String coordinates = "h3";
         System.out.println(squareIsWhite(coordinates));
     }
 
     public static boolean squareIsWhite(String coordinates) {
-        return true;
+        char[] arr = coordinates.toCharArray();
+        int c1 = arr[0] - 'a';
+        int c2 = arr[1] - '1';
+        return ((c1 + c2) & 1) == 1;
     }
 }
