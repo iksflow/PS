f9a68e24ffd0ef99de19417eff2b67f2681ea511
iksflow <theiksflow@gmail.com>
2021-05-28 23:07:48 +0900

    Leetcode Study 2021.05.27 / P1859 해결.

--- src/leetcode/P1859.java
+++ src/leetcode/P1859.java
@@ -2,10 +2,20 @@ package leetcode;
 
 public class P1859 {
     public static void main(String[] args) {
-
+        String s = "is2 sentence4 This1 a3";
+//        System.out.println(s.substring(0, 2));
+        System.out.println(sortSentence(s));
     }
 
     public static String sortSentence(String s) {
+        String[] strArr = s.split(" ");
+        String[] result = new String[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            int len = strArr[i].length();
+            int idx = strArr[i].charAt(len - 1);
+            result[idx] = strArr[i].substring(0, len - 1);
+        }
+        System.out.println(result.toString());
         return "";
     }
 }
