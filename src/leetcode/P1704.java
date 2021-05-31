fbe9e95cb55f1e36eb752d784a67a8882fbd4288
iksflow <theiksflow@gmail.com>
2021-05-31 23:57:00 +0900

    Leetcode Study 2021.05.31 / P1704 solving

---
+++ src/leetcode/P1704.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class P1704 {
+    public static void main(String[] args) {
+        String s = "book";
+        System.out.println(halvesAreAlike(s));
+    }
+
+    public static boolean halvesAreAlike(String s) {
+        return false;
+    }
+}
