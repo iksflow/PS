3e02adcadd845e5ec443c7d1d9c1742519a17d3f
iksflow <theiksflow@gmail.com>
2021-06-03 23:40:15 +0900

    Leetcode Study 2021.06.03 / P1725 solving

---
+++ src/leetcode/P1725.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class P1725 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int countGoodRectangles(int[][] rectangles) {
+        return 0;
+    }
+}
