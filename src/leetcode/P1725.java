7c02bef7d26396572caade01628d0c4d61ed57b1
iksflow <theiksflow@gmail.com>
2021-06-03 23:53:19 +0900

    Leetcode Study 2021.06.03 / P1725 solved

--- src/leetcode/P1725.java
+++ src/leetcode/P1725.java
@@ -2,10 +2,24 @@ package leetcode;
 
 public class P1725 {
     public static void main(String[] args) {
-
+        int[][] rectangles = {{5,8},{3,9},{5,12},{16,5}};
+        System.out.println(countGoodRectangles(rectangles));
     }
 
     public static int countGoodRectangles(int[][] rectangles) {
-        return 0;
+        int max = 0;
+        int result = 0;
+        for (int i = 0; i < rectangles.length; i++) {
+            int[] rectangle = rectangles[i];
+            int width = Math.min(rectangle[0], rectangle[1]);
+            if (width == max) {
+                result ++;
+            } else if (max < width) {
+                max = width;
+                result = 1;
+            }
+        }
+
+        return result;
     }
 }
