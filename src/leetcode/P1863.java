2f1094d55959e3d783ac970e7f315d0f3abb4aa8
iksflow <theiksflow@gmail.com>
2021-06-04 22:14:38 +0900

    2021.06.04 Leetcode Study P1863 Solving

---
+++ src/leetcode/P1863.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class P1863 {
+    public static void main(String[] args) {
+        int[] nums = {1, 3};
+        System.out.println(subsetXORSum(nums));
+    }
+
+    public static int subsetXORSum(int[] nums) {
+        return 0;
+    }
+}
