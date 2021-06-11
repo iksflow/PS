a82e7a51e85c674bf155b3da55a0fe70b4696ad3
iksflow <theiksflow@gmail.com>
2021-06-11 23:55:29 +0900

    2021.06.11  Leetcode Study

---
+++ src/leetcode/P1305.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P1305 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> result = new ArrayList<>();
+
+        return result;
+    }
+}
