b12fb302935a295c12769522fdcdb388525fb65d
iksflow <theiksflow@gmail.com>
2021-06-12 00:27:00 +0900

    2021.06.12 Leetcode Study - 1305 solved  but too slow...

--- src/leetcode/P1305.java
+++ src/leetcode/P1305.java
@@ -1,7 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class P1305 {
     public static void main(String[] args) {
@@ -11,6 +10,17 @@ public class P1305 {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
         List<Integer> result = new ArrayList<>();
 
+        Queue<TreeNode> q1 = new LinkedList<>();
+        q1.add(root1);
+        while (!q1.isEmpty()) {
+            TreeNode node = q1.poll();
+            if (node != null) {
+                result.add(node.val);
+                q1.add(node.left);
+                q1.add(node.right);
+            }
+        }
+        Collections.sort(result);
         return result;
     }
 }
