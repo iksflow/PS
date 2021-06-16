408ad422cb7b02eb0f985ff37e5ef214754e03df
iksflow <theiksflow@gmail.com>
2021-06-16 23:58:39 +0900

    Leetcode Study 2021.06.16 / P1880 solving

---
+++ src/leetcode/P1880.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class P1880 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+        return false;
+    }
+}
