edf1f9842b6c20c4879b55ac0b94276dc3f8b24d
iksflow <theiksflow@gmail.com>
2021-06-23 22:51:13 +0900

    Leetcode Study 2021.06.23 / P1641 Solving

---
+++ src/leetcode/P1641.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class P1641 {
+    public static void main(String[] args) {
+
+    }
+    /*
+    * D(1) = 5
+    * D(2) = D(1) + D(1) - 1 + D(1) - 2 + D(1) - 3 + D(1) - 4 = D(1) * 5 - 10 = 15
+    * D(3) = D(2) * 5 - 10*/
+    public int countVowelStrings(int n) {
+        return 0;
+    }
+}
