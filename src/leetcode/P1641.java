497ce2301142b748716e5cbc259f3c14e3ceb598
iksflow <theiksflow@gmail.com>
2021-06-26 23:59:42 +0900

    Leetcode Study 2021.06.26 / P1880 Solved

--- src/leetcode/P1641.java
+++ src/leetcode/P1641.java
@@ -9,6 +9,8 @@ public class P1641 {
     * D(1) = 5
     * D(2) = D(1) + D(1) - 1 + D(1) - 2 + D(1) - 3 + D(1) - 4 = D(1) * 5 - 10 = 15
     * D(3) = D(2) * 5 - 10*/
+
+//    D(1) = d
     public static int countVowelStrings(int n) {
         int dp[] = new int[5];
 
