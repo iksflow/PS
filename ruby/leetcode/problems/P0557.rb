5f96221e32cb9a5df349611663fe75671f5575e0
iksflow <theiksflow@gmail.com>
2022-03-11 08:56:33 +0900

    [Leetcode][Easy][Solved][TwoPointers] # 557. Reverse Words in a String III

--- ruby/leetcode/problems/P0557.rb
+++ ruby/leetcode/problems/P0557.rb
@@ -1,1 +1,27 @@
+# 557. Reverse Words in a String III
 
+# @param {String} s
+# @return {String}
+def reverse_words(s)
+  len = s.size
+  return s if len == 1
+  left = 0
+  right = 0
+  res = ''
+  while left < len
+    right += 1
+    if (s[right].eql?' ') || (right == len)
+      idx = right - 1
+      while left <= idx
+        res += s[idx]
+        idx -= 1
+      end
+      res += ' ' if right != len
+      left = right + 1
+    end
+  end
+  res
+end
+
+s = 'abc def'
+puts reverse_words(s)
